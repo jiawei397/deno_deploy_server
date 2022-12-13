497bfbe041cd4bb4f6bbd925d4f6f71b99c29ed2
jw397 <jw397@126.com>
2022-12-13 12:12:08 +0800

    ci: use proxy web

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,6 @@ module.exports = {
     name: "deno_deploy_server",
     script: ' ',
     interpreter: "deno",
-    interpreter_args: "task start"
+    interpreter_args: "run --allow-net --allow-env --allow-write --allow-read --allow-run --importmap https://deno-mirror.uino.cn/https/deno.land/x/deno_deploy_server@v1.0.0/import_map_proxy.json --unstable  https://deno-mirror.uino.cn/https/deno.land/x/deno_deploy_server@v1.0.0/mod.ts"
   }]
 };
